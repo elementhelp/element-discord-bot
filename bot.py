77266e892316cb84bb618ead1585a6e475cc08e5
element <elementonline21@gmail.com>
2025-09-04 19:31:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
+import os
 import discord
 from discord.ext import commands
-import os
 
-TOKEN = os.getenv("DISCORD_TOKEN")  # cheia o pui în Render -> Environment
+# luăm tokenul din variabilele Railway (ENV)
+TOKEN = os.getenv("DISCORD_TOKEN")
 
+# intențiile botului
 intents = discord.Intents.default()
-intents.message_content = True  # ai nevoie pt comenzi text
+intents.message_content = True  # ca să poată citi mesaje
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Botul este online ca {bot.user}!")
+    print(f"✅ Botul este online ca {bot.user}")
 
 @bot.command()
 async def ping(ctx):
