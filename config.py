6b531f9028ebbc3cf2b88d1d3a8b83411ec81b66
element <elementonline21@gmail.com>
2025-09-09 16:45:26 +0300

    Update and rename proxy.py to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
